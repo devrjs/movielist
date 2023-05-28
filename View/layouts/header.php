19860446bf5d4e8dcd8cec911833d5e2e4f52c93
Vitor Gomes <vitorjgomes25@gmail.com>
2023-05-28 16:39:08 -0300

    update login page

--- View/layouts/header.php
+++ View/layouts/header.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieList</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="View/css/estilo.css">
 </head>
 <body>
--- View/modules/loginUsuario.php
+++ View/modules/loginUsuario.php
@@ -1,24 +1,24 @@
-<div class="container">
-    <div class="row justify-content-center mt-5">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                <h4>Login</h4>
-                </div>
-                <div class="card-body">
-                <form>
-                    <div class="form-group">
-                    <label for="username">Usuário:</label>
-                    <input type="text" class="form-control" id="username" placeholder="Digite seu nome de usuário">
-                    </div>
-                    <div class="form-group">
-                    <label for="password">Senha:</label>
-                    <input type="password" class="form-control" id="password" placeholder="Digite sua senha">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Entrar</button>
-                </form>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="background-image"></div>
+<div class="text-center ct">
+<form class="form-signin">
+    <h1 class="h3 mb-3 font-weight-normal text-white">Sign In</h1>
+    <input
+    type="email"
+    id="inputEmail"
+    class="form-control"
+    placeholder="Username"
+    required
+    autofocus
+    />
+    <input
+    type="password"
+    class="form-control"
+    placeholder="Password"
+    required
+    />
+    <button type="submit" class="btn btn-lg btn-primary btn-block">
+    Entrar
+    </button>
+</form>
+<p class="mt-5 mb-3 text-white">&copy; 2023</p>
 </div>
